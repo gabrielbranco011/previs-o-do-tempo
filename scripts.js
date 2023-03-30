75e5fcd001489be52747fa9d200ecef8bd59c061
Gabriel Branco <gabriel.live.2012@hotmail.com>
2023-03-30 02:55:23 -0300

    add dados queryselector

--- scripts.js
+++ scripts.js
@@ -2,12 +2,19 @@
 
 const key = "b743f91feef37e9ec5eb7d5c41dc9fd8"
 
+function colocarDadosNaTela(dados) {
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+}
 
 async function BuscarCidade(cidade) {
 
-const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`).then( resposta => resposta.json())
+const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
 
-    console.log(dados)
+    colocarDadosNaTela(dados)
 }
 
 function cliqueiNoBotao() {
